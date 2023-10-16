95d08a1ca71cbe6a6db6e366a1bfb1cd47a5ad5e
Arvin Rolos <arolos@bcit.ca>
2023-10-16 14:12:45 -0700

    fix: hintjs warnings

--- source/js/features/elastic-apm-rum.js
+++ source/js/features/elastic-apm-rum.js
@@ -1,83 +1,88 @@
-const urlObj = new URL(window.location.href);
-const urlHostname = urlObj.hostname; // 'learn.bcit.ca'
-const urlProtocol = urlObj.protocol; // 'https:', 'http:', 'file:', etc.
+(function ($) {
+    function initSugarApm() {
+        const urlObj = new URL(window.location.href);
+        const urlHostname = urlObj.hostname; // 'learn.bcit.ca'
+        const urlProtocol = urlObj.protocol; // 'https:', 'http:', 'file:', etc.
 
-const ignoreRegexList = [
-    /^localhost$/,
-    /^review--.*\.ltc\.bcit\.ca$/,
-    /^latest--.*\.ltc\.bcit\.ca$/
-];
+        const ignoreRegexList = [
+            /^localhost$/,
+            /^review--.*\.ltc\.bcit\.ca$/,
+            /^latest--.*\.ltc\.bcit\.ca$/
+        ];
 
-var isIgnore = false;
+        var isIgnore = false;
 
-if (urlProtocol === 'file:') {
-    isIgnore = true;
-} else {
-    for (const regex of ignoreRegexList) {
-        if (regex.test(urlHostname)) {
+        if (urlProtocol === 'file:') {
             isIgnore = true;
-            break;
-        }
-    }
-}
-
-if (isIgnore) {
-    console.log(`Ignoring ${urlHostname}`);
-} else {
-    const urlHref = urlObj.href; // 'https://learn.bcit.ca/content/enforced/123456-course/about.html?d2lSessionVal=abcd1234&ou=123456&d2l_body_type=3'
-    const urlPort = urlObj.port; // '8080'. (443, 80, and non-explicit ports will return empty string)
-    const urlParams = urlObj.search; // '?d2lSessionVal=abcd1234&ou=123456&d2l_body_type=3'
-    const urlPathname = urlObj.pathname; // '/content/enforced/123456-course/about.html' (excludes query params)
-
-    const courseLocAndPathRegex = /\/content\/enforced\/([^\/]+)(\/.*)/;
-    const matches = urlPathname.match(courseLocAndPathRegex);
-    let courseLocation = null;
-    let manageFilePath = null;
-    if (matches && matches[1] && matches[2]) {
-        courseLocation = matches[1];
-        manageFilePath = matches[2];
-    };
-
-    const myApm = elasticApm.init({
-        serviceName: 'sugar-suite',
-        serverUrl: 'https://a7522a4e02d843c3a373ba62f6fc5436.apm.westus2.azure.elastic-cloud.com:443',
-        breakdownMetrics: true
-    });
-    const transaction = myApm.getCurrentTransaction();
-    let transactionLabels = {
-        "urlPort": urlPort,
-    };
-    if (manageFilePath) {
-        transactionLabels["manageFilePath"] = manageFilePath;
-    }
-    if (transaction && !transaction.ended) {
-        if (courseLocation) {
-            transaction.name = courseLocation;
-
-            const parts = courseLocation.split('-');
-            if (parts.length > 0) {
-                const firstPart = parts[0];
-                if (/^\d+$/.test(firstPart)) {
-                    transactionLabels["courseOrgUnit"] = firstPart;
+        } else {
+            for (const regex of ignoreRegexList) {
+                if (regex.test(urlHostname)) {
+                    isIgnore = true;
+                    break;
                 }
             }
+        }
 
-        } else if (urlHostname === "sugar-suite.ltc.bcit.ca") {
-            transaction.name = "stable-sugar-suite";
-        } else if (urlHostname === "conversion-guide.ltc.bcit.ca") {
-            transaction.name = "stable-conversion-guide";
+        if (isIgnore) {
+            console.log(`Ignoring ${urlHostname}`);
         } else {
-            transaction.name = urlHref;
+            const urlHref = urlObj.href; // 'https://learn.bcit.ca/content/enforced/123456-course/about.html?d2lSessionVal=abcd1234&ou=123456&d2l_body_type=3'
+            const urlPort = urlObj.port; // '8080'. (443, 80, and non-explicit ports will return empty string)
+            const urlParams = urlObj.search; // '?d2lSessionVal=abcd1234&ou=123456&d2l_body_type=3'
+            const urlPathname = urlObj.pathname; // '/content/enforced/123456-course/about.html' (excludes query params)
+
+            const courseLocAndPathRegex = /\/content\/enforced\/([^\/]+)(\/.*)/;
+            const matches = urlPathname.match(courseLocAndPathRegex);
+            let courseLocation = null;
+            let manageFilePath = null;
+            if (matches && matches[1] && matches[2]) {
+                courseLocation = matches[1];
+                manageFilePath = matches[2];
+            }
+
+            const myApm = elasticApm.init({
+                serviceName: 'sugar-suite',
+                serverUrl: 'https://a7522a4e02d843c3a373ba62f6fc5436.apm.westus2.azure.elastic-cloud.com:443',
+                breakdownMetrics: true
+            });
+            const transaction = myApm.getCurrentTransaction();
+            let transactionLabels = {
+                "urlPort": urlPort,
+            };
+            if (manageFilePath) {
+                transactionLabels.manageFilePath = manageFilePath;
+            }
+            if (transaction && !transaction.ended) {
+                if (courseLocation) {
+                    transaction.name = courseLocation;
+
+                    const parts = courseLocation.split('-');
+                    if (parts.length > 0) {
+                        const firstPart = parts[0];
+                        if (/^\d+$/.test(firstPart)) {
+                            transactionLabels.courseOrgUnit = firstPart;
+                        }
+                    }
+
+                } else if (urlHostname === "sugar-suite.ltc.bcit.ca") {
+                    transaction.name = "stable-sugar-suite";
+                } else if (urlHostname === "conversion-guide.ltc.bcit.ca") {
+                    transaction.name = "stable-conversion-guide";
+                } else {
+                    transaction.name = urlHref;
+                }
+
+                const urlParamsObj = new URLSearchParams(urlParams);
+                for (const [key, value] of urlParamsObj) {
+                    transactionLabels[`query_${key}`] = value;
+                }
+
+                transaction.addLabels(transactionLabels);
+
+            } else {
+                console.log("No active transaction or transaction has already ended.");
+            }
         }
-
-        const urlParamsObj = new URLSearchParams(urlParams);
-        for (const [key, value] of urlParamsObj) {
-            transactionLabels[`query_${key}`] = value;
-        }
-
-        transaction.addLabels(transactionLabels);
-
-    } else {
-        console.log("No active transaction or transaction has already ended.");
     }
-}
+    initSugarApm();
+}(jQuery));
