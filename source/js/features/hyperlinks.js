b6a08d5d2c3555c94c95785b6a1e952bbb43c2f8
Arvin Rolos <arolos@bcit.ca>
2024-04-10 08:35:48 -0700

    fix: rewrite hyperlinks without href or with _self to _blank

--- source/js/features/hyperlinks.js
+++ source/js/features/hyperlinks.js
@@ -14,53 +14,59 @@
 		"pdf",
 		"mp4"
 	];
-	
+
 	// Expression for selecting specific files
 	$.expr[':'].browserFiles = function (link) {
-		if(link.pathname && link.pathname.length) {
+		if (link.pathname && link.pathname.length) {
 			var pathname = link.pathname.toLowerCase();
 			var ext = pathname.substring(pathname.lastIndexOf(".") + 1);
-			
-			if(browserFiles.indexOf(ext) !== -1) {
+
+			if (browserFiles.indexOf(ext) !== -1) {
 				return true;
 			}
 		}
 		return false;
 	};
-	
+
 	// Address old content affected by the Dewordify bug that prepended email addresses with an assets folder
-	$("a[href*='assets/mailto:']").each(function() {
+	$("a[href*='assets/mailto:']").each(function () {
 		var href = $(this).attr("href");
 		var newHref = href.split("assets/")[1];
-		$(this).attr("href",newHref);
+		$(this).attr("href", newHref);
 	});
-	
+
 	// Expression for selecting external links
 	$.expr[':'].external = function (link) {
 		return !link.href.match(/^mailto\:/) &&
 			(link.hostname != location.hostname);
 	};
-	
-	// Set all anchors to open in new tab
-	$("a").attr("target", "_top");
 
-	
-	// Set specific files types to open in a new tab
-	$("a:browserFiles").each(function() {
-		$(this).attr("target","_blank");
-		$(this).attr("rel", "external");
-		$(this).attr("title", "Opens in a new tab");
+	// Set all hyperlink with no target to open link in new tab
+	$("a").each(function () {
+		if (!$(this).attr("target")) {
+			let linkHref = $(this).attr("href");
+			$(this).attr("target", "_blank");
+		}
 	});
-	
-	
+
+	// Set specific files types to open in a new tab
+	$("a:browserFiles").each(function () {
+		if ($(this).attr("target") == "_self") {
+			$(this).attr("target", "_blank");
+			$(this).attr("rel", "external");
+			$(this).attr("title", "Opens in a new tab");
+		}
+	});
+
 	// Set external anchors to open in new tab
 	$("a:external").each(function () {
-		$(this).attr("target", "_blank");
-		$(this).attr("rel", "external");
-		$(this).attr("title", "Opens in a new tab");
+		if ($(this).attr("target") == "_self") {
+			$(this).attr("target", "_blank");
+			$(this).attr("rel", "external");
+			$(this).attr("title", "Opens in a new tab");
+		}
 	});
-	
-	
+
 	// Set title of anchors with download attribute
-	$("a[download]").attr("title","Download");
+	$("a[download]").attr("title", "Download");
 }(jQuery));
