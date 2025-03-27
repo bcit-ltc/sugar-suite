39396ff46a32506ece7fc3b797f67ab3aa644bd4
Arvin Rolos <arolos@bcit.ca>
2025-03-27 11:59:58 -0700

    test

--- dagger/src/sugar_suite/main.py
+++ dagger/src/sugar_suite/main.py
@@ -9,16 +9,20 @@ class SugarSuite:
 
     # filepath: c:\Users\arolos\Desktop\github\sugar-suite\dagger\src\sugar_suite\main.py
     @function
-    async def publish(self, source: Annotated[dagger.Directory, DefaultPath("./")], registry: str, username: str, tags: str) -> str:
+    async def publish(self, source: Annotated[dagger.Directory, DefaultPath("./")], registry: str, username: str, password: dagger.Secret, tags: str) -> str:
         """Publish the application container to a registry"""
         # Split the tags by comma and strip any whitespace
         tag_list = [t.strip() for t in tags.split(",")]
     
         # Load the password secret from the environment
-        password_secret = dagger.Secret.from_env("GITHUB_TOKEN")
+        
     
         # Call Dagger Function to build the application image
-        image = self.build(source).with_registry_auth(registry, username, password_secret)
+        image = (
+            self.build(source)
+            .with_secret_variable("GITHUB_TOKEN", password)
+            .with_registry_auth(registry, username, password)
+            )
     
         # Publish the image for each tag
         for tag in tag_list:
