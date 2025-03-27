2dcfa1a2388138cf92eb74f2b561ec6f4c1e4d03
Arvin Rolos <arolos@bcit.ca>
2025-03-27 12:45:16 -0700

    test77

--- dagger/src/sugar_suite/main.py
+++ dagger/src/sugar_suite/main.py
@@ -7,7 +7,7 @@ from dagger import DefaultPath, dag, function, object_type, Secret
 class SugarSuite:
 
     @function
-    async def publish(self, source: Annotated[dagger.Directory, DefaultPath("./")], registry: str, username: str, password: str, tags: str) -> str:
+    async def publish(self, source: Annotated[dagger.Directory, DefaultPath("./")], registry: str, username: str, password: dagger.Secret, tags: str) -> str:
         """Publish the application container to a registry"""
         # Split the tags by comma and strip any whitespace
         tag_list = [t.strip() for t in tags.split(",")]
