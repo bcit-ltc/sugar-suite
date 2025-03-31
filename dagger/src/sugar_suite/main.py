4d8b952329a052d0c152fdb908fcb128a9798b24
Arvin Rolos <arolos@bcit.ca>
2025-03-31 11:41:09 -0700

    with_secret_variable

--- dagger/src/sugar_suite/main.py
+++ dagger/src/sugar_suite/main.py
@@ -58,7 +58,7 @@ class SugarSuite:
             .with_exec(["git", "config", "--global", "user.name", "github-actions[bot]"])
             .with_exec(["git", "config", "--global", "user.email", "github-actions[bot]@users.noreply.github.com"])
             # Set the GITHUB_TOKEN environment variable
-            .with_env_variable("GITHUB_TOKEN", token)
+            .with_secret_variable("GITHUB_TOKEN", token)
             # Copy all files from dependencies_container except node_modules
             .with_directory("/usr/share/nginx/html/.git", source.directory(".git"))
             # Preserve the pre-installed node_modules in the semantic-release container
