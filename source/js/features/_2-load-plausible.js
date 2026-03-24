a47f7ce3426e745abff7bb6635b4c2984b445f01
Arvin Rolos <arolos@bcit.ca>
2026-03-24 13:03:05 -0700

    change pageAndScript to scriptLocation

--- source/js/features/_2-load-plausible.js
+++ source/js/features/_2-load-plausible.js
@@ -1,9 +1,9 @@
-(function() {
+(function () {
 	// When any page loads lat.js, send a custom event via LTC Plausible (utils.js).
 	// Uses common.ltc.bcit.ca/js/utils.js so adblockers don't block it.
-	window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments); };
+	window.plausible = window.plausible || function () { (window.plausible.q = window.plausible.q || []).push(arguments); };
 	if (!window.plausible.o) window.plausible.o = { captureOnLocalhost: true, autoCapturePageviews: true };
-	if (!window.ltcDomain) window.ltcDomain = { eventDomain: 'sugar-suite.latest.ltc.bcit.ca' };
+	if (!window.ltcDomain) window.ltcDomain = { eventDomain: 'sugar-suite.ltc.bcit.ca' };
 	function nonEmpty(value) {
 		if (value === null || value === undefined) return '';
 		return String(value).trim();
@@ -36,7 +36,7 @@
 			}
 		}
 	}
-	
+
 	var computedLatScriptUrl = '';
 	try {
 		computedLatScriptUrl = new URL('js/lat.js', document.baseURI || window.location.href).href;
@@ -45,7 +45,7 @@
 	}
 	latScriptUrl = nonEmpty(latScriptUrl) || nonEmpty(computedLatScriptUrl);
 	if (!pageUrl) return;
-	var pageAndScript = pageUrl + ' | ' + latScriptUrl;
+	var scriptLocation = JSON.stringify({ page: pageUrl, scriptUrl: latScriptUrl });
 
 	// Avoid sending duplicate events from repeated initializations.
 	var dedupeHost = window;
@@ -55,11 +55,11 @@
 		dedupeHost = window;
 	}
 	dedupeHost.__latPlausibleEvents = dedupeHost.__latPlausibleEvents || {};
-	if (dedupeHost.__latPlausibleEvents[pageAndScript]) return;
-	dedupeHost.__latPlausibleEvents[pageAndScript] = true;
+	if (dedupeHost.__latPlausibleEvents[scriptLocation]) return;
+	dedupeHost.__latPlausibleEvents[scriptLocation] = true;
 
-	plausible('lat.js loaded', { props: { page: pageUrl, scriptUrl: latScriptUrl, pageAndScript: pageAndScript } });
-	
+	plausible('lat.js location', {props: { scriptUrl: latScriptUrl, scriptLocation: scriptLocation }});
+
 	var s = document.createElement('script');
 	s.defer = true;
 	s.src = 'https://common.ltc.bcit.ca/js/utils.js';
